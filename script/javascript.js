ac56fcde375de142f8ed6267714232e54189ed59
Kenneth <rekerc4@gmail.com>
2018-07-25 17:58:57 -0400

    Final Commit

--- script/javascript.js
+++ script/javascript.js
@@ -8,7 +8,8 @@ class AddressBook {
         this.contact.splice(this.contact.length, 0, info);
     }
     deleteAt(index){
-        this.contact.splice(index, 1); 
+        let theIndex = parseInt(index);
+        this.contact.splice(theIndex, 1); 
         console.log(book)
     }
     print(){
@@ -20,8 +21,12 @@ class AddressBook {
 
     }
     deleteByName(name){
+        name = name.toLowerCase().trim(); 
         for(let i in this.contact){
-            if(this.contact[i] == name){
+            let com = this.contact[i].name.toLowerCase().trim();
+            console.log(com); 
+            console.log(name);
+            if(com == name){
                 this.contact.splice(i, 1); 
             }
         }
@@ -29,7 +34,10 @@ class AddressBook {
     }
     updateByName(name, newNumber, newEmail){
         for(let i in this.contact){
-            if(this.contact[i] == name){
+            let com = this.contact[i].name.toLowerCase().trim();
+            console.log(com); 
+            console.log(name);
+            if(com == name){
                 if(!newNumber){
 
                 }
@@ -56,38 +64,6 @@ class Contact {
     }
 }
 
-let tree = (p) => {
-    p = p.toLocaleLowerCase().trim(); 
-    let rep = p.replace(/[\W0-9_]+/g, ''); 
-    if(rep === "a"){
-        let n = prompt("What is the name of your new contact?");
-        let ph = prompt("What is the phone number of your new contact?");
-        let e = prompt("What is the email of your new contact?");
-        let r = prompt("What is the relation of your new contact?");
-        book.add(new Contact(n, ph, e, r, p));
-        let p = prompt("Add, Remove, Print, or Quit?");
-        tree(p); 
-       
-    }
-    else if(rep === "r"){
-        let p = prompt("Add, Remove, Print, or Quit?");
-    }
-    else if(rep === "p"){
-        let p = prompt("Add, Remove, Print, or Quit?");
-    }
-    else if(rep === "q"){
-        return; 
-    }
-    else{
-        p = prompt("I didnt catch that please enter either print, add, remove, update, or quit.")
-    }
-}
-
-let p = prompt("Add, Remove, Print, or Quit?");
-
-tree(p);
-
-
 const book = new AddressBook();
 
 book.add(new Contact("You", "you@you.org", 2345679876, "you")); 
@@ -99,7 +75,68 @@ console.log(book);
 
 book.deleteAt(1); 
 
-book.print();
+book.deleteByName("You");
 
-book.deleteByName
+book.updateByName("Those", 586738475233, "those@those.io");
+
+
+let eithOr = (nOrIn) => {
+    nOrIn = nOrIn.toLowerCase().trim(); 
+    let sel = nOrIn.replace(/[\W0-9_]+/g, '').slice(0, 1); 
+    if(sel === "n"){
+        let whatName = prompt("What name do you want to delete?");
+        book.deleteByName(whatName); 
+    }
+    else if(sel === "i"){
+        let whatIndex = prompt("What index do you want to delete?");
+        book.deleteAt(whatIndex);
+        let p = prompt("Add, Remove, Print, or Quit?");
+        tree(p); 
+    }
+    else{
+      let plSelRight = prompt("Select either index or name!");   
+      eithOr(plSelRight);  
+    }
+}
+
+let tree = (p) => {
+    p = p.toLowerCase().trim(); 
+    let rep = p.replace(/[\W0-9_]+/g, '').slice(0, 1); 
+    if(rep === "a"){
+        let n = prompt("What is the name of your new contact?");
+        let ph = prompt("What is the phone number of your new contact?");
+        let e = prompt("What is the email of your new contact?");
+        let r = prompt("What is the relation of your new contact?");
+        book.add(new Contact(n, ph, e, r, p));
+        p = prompt("Add, Remove, Print, or Quit?");
+        tree(p); 
+       
+    }
+    else if(rep === "r"){
+        let nOrIn = prompt("Do you want to remove by index or name?");
+        eithOr(nOrIn); 
+        p = prompt("Add, Remove, Print, or Quit?");
+        tree(p);
+
+    }
+    else if(rep === "p"){
+        book.print(); 
+        p = prompt("Add, Remove, Print, or Quit?");
+        tree(p);
+    }
+    else if(rep === "q"){
+        return; 
+    }
+    else{
+        p = prompt("I didnt catch that please enter either print, add, remove, update, or quit."); 
+        tree(p); 
+    }
+}
+
+let p = prompt("Add, Remove, Print, or Quit?");
+
+tree(p);
+
+
+
 
